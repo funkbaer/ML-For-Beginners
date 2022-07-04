5d14963b516ead7f71bb6e330cd9f80e33329d20
Tim Geerken <tim.geerken@ipt.fraunhofer.de>
2022-07-04 08:58:13 +0200

    NLP chapter 3

--- 6-NLP/2-Tasks/test.py
+++ 6-NLP/2-Tasks/test.py
@@ -3,8 +3,51 @@ from textblob.np_extractors import ConllExtractor
 # import and create a Conll extractor to use later 
 extractor = ConllExtractor()
 
+
 # later when you need a noun phrase extractor:
-user_input = input("> ")
-user_input_blob = TextBlob(user_input, np_extractor=extractor)  # note non-default extractor specified
-np = user_input_blob.noun_phrases
-print(np)
+
+lst_prior = []
+
+def user_input():
+
+    user_input = input("> ")
+    user_input_blob = TextBlob(user_input, np_extractor=extractor)  # note non-default extractor specified
+    np = user_input_blob.noun_phrases
+
+    if user_input == "exit":
+        return False
+    if user_input_blob.polarity <= -0.5:
+        response = "Oh dear, that sounds bad. "
+    elif user_input_blob.polarity <= 0:
+        response = "Hmm, that's not great. "
+    elif user_input_blob.polarity <= 0.5:
+        response = "Well, that sounds positive. "
+    elif user_input_blob.polarity <= 1:
+        response = "Wow, that sounds great. "
+
+    for item in np:
+        if item in lst_prior:
+            print(f"Oh you already told me about {item} that's nice to hear" )
+            return True
+    
+    if len(np) == 0:
+        print( response + "Can you tell me more?")
+    else:
+        print( response + f"Can you tell me more about {np[0].pluralize()} ?")
+
+    for item in np:
+        lst_prior.append(item)
+
+    return True
+
+
+bot_running = True
+
+print("Hello, my name is simple bot.")
+print("You can end this conversation by simple typing: exit")
+print("After typing each answer, press 'enter'")
+print("How are you today?")
+
+while bot_running:
+    bot_running = user_input()
+
